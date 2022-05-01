43b7130d3a6984098e22c43ed411a5c907a15d7e
mactav1sh <mactavish104@gmail.com>
2022-05-01 22:26:51 +0200

    Added more unit tests and improved code structure

--- src/tests/productSpec.ts
+++ src/tests/productSpec.ts
@@ -7,146 +7,161 @@ import supertest from 'supertest';
 const store = new productStore();
 const request = supertest(app);
 
-describe('Products - Testing for the Existance of Methods', () => {
-  it('Should have an index method', () => {
-    expect(store.index).toBeDefined();
-  });
-  it('Should have a show method', () => {
-    expect(store.showProduct).toBeDefined();
-  });
-  it('Should have a create method', () => {
-    expect(store.createProduct).toBeDefined();
-  });
-});
-
-describe('Products Endpoints Testing', () => {
-  const test_product = {
-    name: 'milk',
-    price: 20,
-  } as Product;
-
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdF9uYW1lIjoidGVzdCIsImxhc3RfbmFtZSI6InVzZXIiLCJwYXNzd29yZCI6InBhc3Nhc2QxMjMiLCJpYXQiOjE2NTEyNDg5ODF9.3c7FoZ8qnPQwpLlfsVTLMsMAZHEwgc4eC-T7uTK_leg';
-
-  beforeAll(async () => {
-    const productResponse = await store.createProduct(
-      test_product.name,
-      test_product.price
-    );
-    test_product.id = productResponse.id;
+describe('Products - Unit Testing', () => {
+  describe('Products - Testing for the Existance of Methods', () => {
+    it('Should have an index method', () => {
+      expect(store.index).toBeDefined();
+    });
+    it('Should have a show method', () => {
+      expect(store.showProduct).toBeDefined();
+    });
+    it('Should have a create method', () => {
+      expect(store.createProduct).toBeDefined();
+    });
   });
 
-  it('api/products Endpoint - Create product', async () => {
-    const response = await request
-      .post('/api/products')
-      .set('content-type', 'application/json')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
+  describe('Products Endpoints Testing', () => {
+    const test_product = {
+      name: 'milk',
+      price: 20,
+    } as Product;
+
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdF9uYW1lIjoidGVzdCIsImxhc3RfbmFtZSI6InVzZXIiLCJwYXNzd29yZCI6InBhc3Nhc2QxMjMiLCJpYXQiOjE2NTEyNDg5ODF9.3c7FoZ8qnPQwpLlfsVTLMsMAZHEwgc4eC-T7uTK_leg';
+
+    beforeAll(async () => {
+      const productResponse = await store.createProduct(
+        test_product.name,
+        test_product.price
+      );
+      test_product.id = productResponse.id;
+    });
+
+    it('api/products Endpoint - Create product', async () => {
+      const response = await request
+        .post('/api/products')
+        .set('content-type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          name: 'juice',
+          price: 50,
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual({
+        id: 2,
         name: 'juice',
         price: 50,
       });
-
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual({
-      id: 2,
-      name: 'juice',
-      price: 50,
     });
-  });
 
-  it('api/users/:id Endpoint - Show product', async () => {
-    const response = await request
-      .get('/api/products/1')
-      .set('Authorization', `Bearer ${token}`);
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual({
-      id: test_product.id,
-      name: test_product.name,
-      price: test_product.price,
-    });
-  });
-
-  it('api/products Endpoint - Index', async () => {
-    const response = await request
-      .get('/api/products')
-      .set('Authorization', `Bearer ${token}`);
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual([
-      {
+    it('api/users/:id Endpoint - Show product', async () => {
+      const response = await request
+        .get('/api/products/1')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual({
         id: test_product.id,
         name: test_product.name,
         price: test_product.price,
-      },
-      {
-        id: 2,
-        name: 'juice',
-        price: 50,
-      },
-    ]);
-  });
+      });
+    });
 
-  afterAll(async () => {
-    const connection = await client.connect();
-    await connection.query(
-      `DELETE FROM products; ALTER SEQUENCE products_id_seq RESTART WITH 1;`
-    );
-    connection.release();
-  });
-});
+    it('api/users/:id Endpoint - Show product with "wrong" id should return status 404', async () => {
+      const response = await request
+        .get('/api/products/999')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(404);
+      expect(response.body.data).toBeUndefined();
+    });
 
-describe('Products - Testing results of Model methods', () => {
-  const test_product = {
-    name: 'milk',
-    price: 20,
-  } as Product;
+    it('api/products Endpoint - Index', async () => {
+      const response = await request
+        .get('/api/products')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual([
+        {
+          id: test_product.id,
+          name: test_product.name,
+          price: test_product.price,
+        },
+        {
+          id: 2,
+          name: 'juice',
+          price: 50,
+        },
+      ]);
+    });
 
-  beforeAll(async () => {
-    const productResponse = await store.createProduct(
-      test_product.name,
-      test_product.price
-    );
-    test_product.id = productResponse.id;
-  });
-
-  it('Create method should add a product', async () => {
-    const response = await store.createProduct('juice', 55);
-    expect(response).toEqual({
-      name: 'juice',
-      price: 55,
-      id: 2,
+    afterAll(async () => {
+      const connection = await client.connect();
+      await connection.query(
+        `DELETE FROM products; ALTER SEQUENCE products_id_seq RESTART WITH 1;`
+      );
+      connection.release();
     });
   });
 
-  it('Show product method shoud get a product with a specific id', async () => {
-    const response = await store.showProduct(test_product.id as number);
-    expect(response).toEqual({
-      name: test_product.name,
-      price: test_product.price,
-      id: test_product.id,
-    });
-  });
+  describe('Products - Testing results of Model methods', () => {
+    const test_product = {
+      name: 'milk',
+      price: 20,
+    } as Product;
 
-  it('Index method shoud get all products', async () => {
-    const response = await store.index();
-    expect(response).toEqual([
-      {
-        name: test_product.name,
-        price: test_product.price,
-        id: test_product.id,
-      },
-      {
+    beforeAll(async () => {
+      const productResponse = await store.createProduct(
+        test_product.name,
+        test_product.price
+      );
+      test_product.id = productResponse.id;
+    });
+
+    it('Create method should add a product', async () => {
+      const response = await store.createProduct('juice', 55);
+      expect(response).toEqual({
         name: 'juice',
         price: 55,
         id: 2,
-      },
-    ]);
-  });
+      });
+    });
 
-  afterAll(async () => {
-    const connection = await client.connect();
-    await connection.query(
-      `DELETE FROM products; ALTER SEQUENCE products_id_seq RESTART WITH 1;`
-    );
-    connection.release();
+    it('Show product method shoud get a product with a specific id', async () => {
+      const response = await store.showProduct(test_product.id as number);
+      expect(response).toEqual({
+        name: test_product.name,
+        price: test_product.price,
+        id: test_product.id,
+      });
+    });
+
+    it('Show product method with wrong id should return undefined', async () => {
+      const response = await store.showProduct(999);
+      expect(response).toBeUndefined();
+    });
+
+    it('Index method shoud get all products', async () => {
+      const response = await store.index();
+      expect(response).toEqual([
+        {
+          name: test_product.name,
+          price: test_product.price,
+          id: test_product.id,
+        },
+        {
+          name: 'juice',
+          price: 55,
+          id: 2,
+        },
+      ]);
+    });
+
+    afterAll(async () => {
+      const connection = await client.connect();
+      await connection.query(
+        `DELETE FROM products; ALTER SEQUENCE products_id_seq RESTART WITH 1;`
+      );
+      connection.release();
+    });
   });
 });
--- src/tests/userSpec.ts
+++ src/tests/userSpec.ts
@@ -8,148 +8,158 @@ import client from '../database/database';
 const store = new UserStore();
 const request = supertest(app);
 
-describe('Users - Testing for the Existance of Model Methods', () => {
-  it('Should have an index method', () => {
-    expect(store.index).toBeDefined();
-  });
-  it('Should have a show method', () => {
-    expect(store.showUser).toBeDefined();
-  });
-  it('Should have a create method', () => {
-    expect(store.createUser).toBeDefined();
-  });
-});
-
-describe('Users Endpoints Testing', () => {
-  const test_user = {
-    first_name: 'test',
-    last_name: 'user',
-    password: 'password123',
-  } as User;
-
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdF9uYW1lIjoidGVzdCIsImxhc3RfbmFtZSI6InVzZXIiLCJwYXNzd29yZCI6InBhc3Nhc2QxMjMiLCJpYXQiOjE2NTEyNDg5ODF9.3c7FoZ8qnPQwpLlfsVTLMsMAZHEwgc4eC-T7uTK_leg';
-
-  beforeAll(async () => {
-    const userResponse = await store.createUser(
-      test_user.first_name,
-      test_user.last_name,
-      test_user.password as string
-    );
-    test_user.id = userResponse.id;
-  });
-
-  it('api/users Endpoint - Create user', async () => {
-    const response = await request
-      .post('/api/users')
-      .set('content-type', 'application/json')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        first_name: 'test2',
-        last_name: 'user2',
-        password: 'passasd123',
-      });
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual({
-      id: 2,
-      first_name: 'test2',
-      last_name: 'user2',
+describe('Users - Unit Testing', () => {
+  describe('Users - Testing for the Existance of Model Methods', () => {
+    it('Should have an index method', () => {
+      expect(store.index).toBeDefined();
     });
-  });
-  it('api/users/:id Endpoint - Show user', async () => {
-    const response = await request
-      .get('/api/users/1')
-      .set('Authorization', `Bearer ${token}`);
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual({
-      id: 1,
-      first_name: test_user.first_name,
-      last_name: test_user.last_name,
+    it('Should have a show method', () => {
+      expect(store.showUser).toBeDefined();
+    });
+    it('Should have a create method', () => {
+      expect(store.createUser).toBeDefined();
     });
   });
 
-  it('api/users Endpoint - Index', async () => {
-    const response = await request
-      .get('/api/users')
-      .set('Authorization', `Bearer ${token}`);
-    expect(response.status).toBe(200);
-    expect(response.body.data).toEqual([
-      {
-        id: 1,
-        first_name: 'test',
-        last_name: 'user',
-      },
-      {
+  describe('Users Endpoints Testing', () => {
+    const test_user = {
+      first_name: 'test',
+      last_name: 'user',
+      password: 'password123',
+    } as User;
+
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaXJzdF9uYW1lIjoidGVzdCIsImxhc3RfbmFtZSI6InVzZXIiLCJwYXNzd29yZCI6InBhc3Nhc2QxMjMiLCJpYXQiOjE2NTEyNDg5ODF9.3c7FoZ8qnPQwpLlfsVTLMsMAZHEwgc4eC-T7uTK_leg';
+
+    beforeAll(async () => {
+      const userResponse = await store.createUser(
+        test_user.first_name,
+        test_user.last_name,
+        test_user.password as string
+      );
+      test_user.id = userResponse.id;
+    });
+
+    it('api/users Endpoint - Create user', async () => {
+      const response = await request
+        .post('/api/users')
+        .set('content-type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          first_name: 'test2',
+          last_name: 'user2',
+          password: 'passasd123',
+        });
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual({
         id: 2,
         first_name: 'test2',
         last_name: 'user2',
-      },
-    ]);
-  });
+      });
+    });
+    it('api/users/:id Endpoint - Show user', async () => {
+      const response = await request
+        .get('/api/users/1')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual({
+        id: 1,
+        first_name: test_user.first_name,
+        last_name: test_user.last_name,
+      });
+    });
 
-  afterAll(async () => {
-    const connection = await client.connect();
-    await connection.query(
-      `DELETE FROM users; ALTER SEQUENCE users_id_seq RESTART WITH 1;`
-    );
-    connection.release();
-  });
-});
+    it('api/users/:id Endpoint - Show user with "wrong" id should return status 404', async () => {
+      const response = await request
+        .get('/api/users/999')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(404);
+      expect(response.body.data).toBeUndefined();
+    });
 
-describe('Users - Testing results of Model methods', () => {
-  const test_user = {
-    first_name: 'test',
-    last_name: 'user',
-    password: 'password123',
-  } as User;
+    it('api/users Endpoint - Index', async () => {
+      const response = await request
+        .get('/api/users')
+        .set('Authorization', `Bearer ${token}`);
+      expect(response.status).toBe(200);
+      expect(response.body.data).toEqual([
+        {
+          id: 1,
+          first_name: 'test',
+          last_name: 'user',
+        },
+        {
+          id: 2,
+          first_name: 'test2',
+          last_name: 'user2',
+        },
+      ]);
+    });
 
-  beforeAll(async () => {
-    const userResponse = await store.createUser(
-      test_user.first_name,
-      test_user.last_name,
-      test_user.password as string
-    );
-    test_user.id = userResponse.id;
-  });
-
-  it('Create method should create a user', async () => {
-    const response = await store.createUser('test2', 'user2', 'password123');
-    expect(response).toEqual({
-      first_name: 'test2',
-      last_name: 'user2',
-      id: 2,
+    afterAll(async () => {
+      const connection = await client.connect();
+      await connection.query(
+        `DELETE FROM users; ALTER SEQUENCE users_id_seq RESTART WITH 1;`
+      );
+      connection.release();
     });
   });
 
-  it('Show user method shoud get a user with a specific id', async () => {
-    const response = await store.showUser(test_user.id as number);
-    expect(response).toEqual({
-      first_name: test_user.first_name,
-      last_name: test_user.last_name,
-      id: test_user.id,
+  describe('Users - Testing results of Model methods', () => {
+    const test_user = {
+      first_name: 'test',
+      last_name: 'user',
+      password: 'password123',
+    } as User;
+
+    beforeAll(async () => {
+      const userResponse = await store.createUser(
+        test_user.first_name,
+        test_user.last_name,
+        test_user.password as string
+      );
+      test_user.id = userResponse.id;
     });
-  });
-  it('Index method shoud get all users', async () => {
-    const response = await store.index();
-    expect(response).toEqual([
-      {
+
+    it('Create method should create a user', async () => {
+      const response = await store.createUser('test2', 'user2', 'password123');
+      expect(response).toEqual({
+        first_name: 'test2',
+        last_name: 'user2',
+        id: 2,
+      });
+    });
+
+    it('Show user method shoud get a user with a specific id', async () => {
+      const response = await store.showUser(test_user.id as number);
+      expect(response).toEqual({
         first_name: test_user.first_name,
         last_name: test_user.last_name,
         id: test_user.id,
-      },
-      {
-        first_name: 'test2',
-        last_name: 'user2',
-        id: 2,
-      },
-    ]);
-  });
+      });
+    });
+    it('Index method shoud get all users', async () => {
+      const response = await store.index();
+      expect(response).toEqual([
+        {
+          first_name: test_user.first_name,
+          last_name: test_user.last_name,
+          id: test_user.id,
+        },
+        {
+          first_name: 'test2',
+          last_name: 'user2',
+          id: 2,
+        },
+      ]);
+    });
 
-  afterAll(async () => {
-    const connection = await client.connect();
-    await connection.query(
-      `DELETE FROM users; ALTER SEQUENCE users_id_seq RESTART WITH 1;`
-    );
-    connection.release();
+    afterAll(async () => {
+      const connection = await client.connect();
+      await connection.query(
+        `DELETE FROM users; ALTER SEQUENCE users_id_seq RESTART WITH 1;`
+      );
+      connection.release();
+    });
   });
 });
